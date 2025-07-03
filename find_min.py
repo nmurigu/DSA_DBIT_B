6213b8e58056fc2730075de7d4e0f99d25e9310a
nmurigu <nelson.murigu@strathmore.edu>
2025-07-03 11:34:39 +0300

    Delete find_min.py

--- find_min.py
+++
@@ -1,9 +0,0 @@
-def find_min(lst):
-    if len(lst) == 0:
-        return None
-
-    min_value = lst[0]
-    for num in lst[1:]:
-        if num < min_value:
-            min_value = num
-    return min_value
